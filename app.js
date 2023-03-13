805a2199f7913cdf59af566b222b230ab5b5409a
Teryaev Andrey <teryaev.2004@mail.ru>
2023-03-13 23:24:45 +0300

    add-function-delete-post

--- app.js
+++ app.js
@@ -66,6 +66,11 @@ const addComment = (e) => {
   }
 
   if (!isError) {
+    var id =
+      "id" +
+      new Date().toLocaleTimeString() +
+      Math.random().toString(16).slice(2);
+
     if (dateValue.value.length === 0) {
       let date = new Date();
 
@@ -78,6 +83,7 @@ const addComment = (e) => {
       };
 
       const resData = {
+        id,
         name: nameValue.value,
         rating: ratingValue.value,
         date,
@@ -89,6 +95,7 @@ const addComment = (e) => {
       localStorage.setItem("posts", JSON.stringify(posts));
 
       setHTML(
+        resData.id,
         resData.name,
         resData.rating,
         `Сегодня, ${time.hours}:${time.minutes}`,
@@ -109,6 +116,7 @@ const addComment = (e) => {
       };
 
       const resData = {
+        id,
         name: nameValue.value,
         rating: ratingValue.value,
         date: postDate,
@@ -121,6 +129,7 @@ const addComment = (e) => {
 
       if (postDate.toDateString() === yesterday.toDateString()) {
         setHTML(
+          resData.id,
           resData.name,
           resData.rating,
           `Вчера, ${time.hours}:${time.minutes}`,
@@ -128,6 +137,7 @@ const addComment = (e) => {
         );
       } else if (postDate.toDateString() === today.toDateString()) {
         setHTML(
+          resData.id,
           resData.name,
           resData.rating,
           `Сегодня, ${(time, hours)}:${time.minutes}`,
@@ -135,6 +145,7 @@ const addComment = (e) => {
         );
       } else {
         setHTML(
+          resData.id,
           resData.name,
           resData.rating,
           `${postDate.toLocaleDateString()}-${time.hours}:${time.minutes}`,
@@ -145,7 +156,19 @@ const addComment = (e) => {
   }
 };
 
-const setHTML = (name, rating, date, comment) => {
+const deletePost = (id) => {
+  list.innerHTML = "";
+
+  let filteredPosts = posts.filter((post) => {
+    return post.id !== id;
+  });
+
+  localStorage.setItem("posts", JSON.stringify(filteredPosts));
+
+  showPosts();
+};
+
+const setHTML = (id, name, rating, date, comment) => {
   let html = `
     <div class="item">
     <div>
@@ -159,40 +182,64 @@ const setHTML = (name, rating, date, comment) => {
       </p>
     </div>
     <div>${date}</div>
+    <div class="item__btns">
+    <div class="item__btn" onclick="deletePost('${id}')">
+      <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+      <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+      
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+      
+      <g id="SVGRepo_iconCarrier"> <path d="M4 7H20" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M6 7V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V7" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </g>
+      
+      </svg>
+    </div>
+
+    <svg class="item__btn like" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 20C12 20 21 16 21 9.71405C21 6 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12.7198 5.92016C12.3266 6.32798 11.6734 6.32798 11.2802 5.92016L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5 4 3 6 3 9.71405C3 16 12 20 12 20Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+    </div>
   </div>
     `;
 
   return list.insertAdjacentHTML("beforeend", html);
 };
 
-if (postsStorage.length > 0) {
-  postsStorage.map((post) => {
-    let today = new Date();
-    let postDate = new Date(post.date);
-    let yesterday = new Date(today);
-    yesterday.setDate(yesterday.getDate() - 1);
-    let resDate;
+const showPosts = () => {
+  postsStorage = JSON.parse(localStorage.getItem("posts")) || [];
+  posts = postsStorage;
 
-    const hours = postDate.getHours();
-    const minutes = postDate.getMinutes();
+  if (postsStorage.length > 0) {
+    postsStorage.map((post) => {
+      let today = new Date();
+      let postDate = new Date(post.date);
+      let yesterday = new Date(today);
+      yesterday.setDate(yesterday.getDate() - 1);
+      let resDate;
 
-    const time = {
-      hours: hours.toString().length < 2 ? "0" + hours : hours,
-      minutes: minutes.toString().length < 2 ? "0" + minutes : minutes,
-    };
+      const hours = postDate.getHours();
+      const minutes = postDate.getMinutes();
 
-    if (today.toDateString() === postDate.toDateString()) {
-      resDate = `Сегодня, ${time.hours}:${time.minutes}`;
-    } else if (postDate.toDateString() === yesterday.toDateString()) {
-      resDate = `Вчера, ${time.hours}:${time.minutes}`;
-    } else {
-      resDate = `${postDate.toLocaleDateString()}-${time.hours}:${
-        time.minutes
-      }`;
-    }
+      const time = {
+        hours: hours.toString().length < 2 ? "0" + hours : hours,
+        minutes: minutes.toString().length < 2 ? "0" + minutes : minutes,
+      };
 
-    return setHTML(post.name, post.rating, resDate, post.comment);
-  });
-}
+      if (today.toDateString() === postDate.toDateString()) {
+        resDate = `Сегодня, ${time.hours}:${time.minutes}`;
+      } else if (postDate.toDateString() === yesterday.toDateString()) {
+        resDate = `Вчера, ${time.hours}:${time.minutes}`;
+      } else {
+        resDate = `${postDate.toLocaleDateString()}-${time.hours}:${
+          time.minutes
+        }`;
+      }
+
+      return setHTML(post.id, post.name, post.rating, resDate, post.comment);
+    });
+  }
+};
+
+showPosts();
 
 formSubmitBtn.addEventListener("click", addComment);
